92827c38a44e6fc7bbb7fee9d6a21f44fb65478e
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-09-11 11:52:32 -0400

    feat: docker compose add mongo container

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import Fastify from 'fastify';
+import mongoose from 'mongoose';
 
 const fastify = Fastify({
   logger: true,
 });
+
+mongoose
+  .connect(
+    'mongodb://url-shortener:password@localhost:27017/url-shortener?authSource=admin',
+  )
+  .then(() => console.log('Connected to database'))
+  .catch((e) => {
+    throw e;
+  });
 
 fastify.get('/', function (request, reply) {
   reply.send({ hello: 'world' });
