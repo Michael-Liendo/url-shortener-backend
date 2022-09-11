acbfb7ebb901d076c80b84d253d2f9ddcb93fe14
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-09-10 23:16:25 -0400

    feat: install and configure prettier and eslint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
+  reply.send({ hello: 'world' });
+});
 
 // Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3000 }, function (err) {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
   // Server is now listening on ${address}
-})
+});
